aba0fa5237e1b7353e7026aa5205bb31ffabcc0c
Fearium <jessebaril.programmer@gmail.com>
2016-08-08 20:03:10 -0400

    Add project files.

---
+++ EnterpriseComputingCOMP2007-lab5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EnterpriseComputingCOMP2007_lab5.MvcApplication" Language="C#" %>
---
+++ EnterpriseComputingCOMP2007-lab5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EnterpriseComputingCOMP2007_lab5.Startup))]
+namespace EnterpriseComputingCOMP2007_lab5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
